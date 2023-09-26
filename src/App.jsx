7500ae4f43c03a25f2a30e3c5dbe357ec5328aed
unknown <kaseybrown28@icloud.com>
2023-09-26 17:09:37 -0400

    updated background :(

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import "./styles/index.css";
-// import video from "./assets/background-2.mp4";
+import background from "./assets/background.jpg";
 import { FooterPage } from "./Component/Footer/index";
 import PageHandler from "./Component/PageHandler";
 function App() {
   return (
     <>
       <div className="main">
-{/*         <div className="overlay">
-          <video src={video} autoPlay loop muted />
-        </div> */}
+        <div className="overlay">
+          <img src={background} />
+        </div>
 
         <div className="content">
           <PageHandler />
