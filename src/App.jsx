9245ce1330ef7a46b13f2b01ff9669e6f715e361
brownKasey <130613198+brownKasey@users.noreply.github.com>
2023-09-26 16:50:41 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import "./styles/index.css";
-import video from "./assets/background-2.mp4";
+// import video from "./assets/background-2.mp4";
 import { FooterPage } from "./Component/Footer/index";
 import PageHandler from "./Component/PageHandler";
 function App() {
   return (
     <>
       <div className="main">
-        <div className="overlay">
+{/*         <div className="overlay">
           <video src={video} autoPlay loop muted />
-        </div>
+        </div> */}
 
         <div className="content">
           <PageHandler />
